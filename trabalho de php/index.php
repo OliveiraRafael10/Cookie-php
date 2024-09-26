db0959cacc0291466af64688dca2b300fedcc6ea
OliveiraRafael10 <161748016+OliveiraRafael10@users.noreply.github.com>
2024-09-25 22:01:45 -0300

    Add files via upload

--- trabalho de php/index.php
+++ trabalho de php/index.php
@@ -7,9 +7,15 @@
     <title>Document</title>
 
     <?php
-        if (ISSET($_COOKIE["idiomapg"]))
+        $opcao = isset($_GET["id"]) ? $_GET["id"] : '';
+        
+        if($opcao == "excluir"){
+            Setcookie("idiomapg");
+        }
+        elseif (ISSET($_COOKIE["idiomapg"]))
         {
             include "compara.inc";
+            header ("Location:empresa.php");
         }
     ?>
 
@@ -18,8 +24,8 @@
     <h1>Bem vindo ao site Web Design em Foco</h1>
     <div class="conteudo">
         <p>Escolha o idioma para entrar</p>
-        <a  href="idioma.php?id=brazil"><img src="img/brazil-icon.png"alt= "BRAZIL" width="100"></a>
         <a  href="idioma.php?id=eua"><img src="img/euaa-icon.png" alt="EUA" width="100"></a>
+        <a  href="idioma.php?id=brazil"><img src="img/brazil-icon.png"alt= "BRAZIL" width="100"></a>
         <a  href="idioma.php?id=spain"><img src="img/spain-icon.png" alt="SPAIN" width="100"></a>
     </div>
 </body>
